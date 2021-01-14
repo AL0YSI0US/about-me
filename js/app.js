6e8268ae4d0654d40e00503ff529e537e4a9f414
aloysious <alicia.todd.army@gmail.com>
2021-01-13 17:01:03 -0800

    uploadSave, styles applied to css, applied div class to footer in html

--- js/app.js
+++ js/app.js
@@ -1,11 +1,78 @@
 'use strict';
 
-console.log('hello world!');
+// console.log('hello world!');
 
-var banana = 42;
+// var banana = 42;
 
-if(banana){
-  console.log(banana);
+// if(banana){
+  // console.log(banana);
+// }
+
+// console.log(banana);
+
+// get username and offer them a greeting
+// var username = prompt('Well hello there wanderer, What\'s your name?');
+alert('Greeting\'s ' + username + ', it\'s a pleasure to meet you!');
+
+// write (ask) 5 questions. they MUST accept yes or no in ANY case
+// normalize reponses to reduce coding efforts to .lowerCase();
+// print to console to confirm 
+
+// question one
+var questionOne = prompt('Do you think I live in the Pacific Northwest?').toLowerCase(); 
+
+if(questionOne === 'yes' || questionOne === 'y') {
+  console.log('You nailed it!');
+  alert('You nailed it!'); 
+} else if(questionOne ==='no' || questionOne === 'n') {
+  alert('That\'s not right but I can see this turning around for you.'); 
+} else {
+  alert('Better luck next time ' + username)
+}
+// question two
+var questionTwo = prompt('Would you believe I\'ve ever fought in cages?').toLowerCase(); 
+
+if(questionTwo === 'yes' || questionTwo === 'y') {
+  console.log('You nailed it!');
+  alert('You nailed it!'); 
+} else if(questionTwo ==='no' || questionTwo === 'n') {
+  alert('That\'s not right but I can still see this turning around for you.'); 
+} else {
+  alert('Are you even typing real words in the prompt box? ' + username)
+}
+// question three
+var questionThree = prompt('Do you think I like poke?').toLowerCase(); 
+
+if(questionThree === 'no' || questionOne === 'n') {
+  console.log('Spot on!');
+  alert('Spot on!'); 
+} else if(questionThree ==='no' || questionThree === 'n') {
+  alert('It\'s okay, it\'s not like I told you i didn\'t like it.'); 
+} else {
+  alert('I\'m not like most Seattleites in this regard ' + username)
 }
 
-console.log(banana);
+// question four
+var questionFour = prompt('Do you think I could have been a sponsored skateboarder in the 90s?').toLowerCase(); 
+
+if(questionThree === 'yes' || questionFour === 'y') {
+  console.log('I miss those days!');
+  alert('I miss those days!'); 
+} else if(questionFour ==='no' || questionFour === 'n') {
+  alert('Sorry ' + username + ', that is incorrect.'); 
+} else {
+  alert('Are you here against your will ' + username + '?')
+}
+
+// question five
+var questionFive = prompt('Did someone make you come here?').toLowerCase(); 
+console.log(questionFive);
+
+if(questionFive === 'yes' || questionFive === 'y') {
+  console.log('I wonder who you ticked off...');
+  alert('I wonder who you ticked off...'); 
+} else if(questionFive ==='no' || questionFive === 'n') {
+  alert('Wow ' + username + ', I feel honored.'); 
+} else {
+  alert('Don\'t fret, it\'s all over now. ' + 'Welcome ' + username + '.')
+}
