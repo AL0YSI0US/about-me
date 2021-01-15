31ad2d3206bea3791eb44c51ea18425c30e8cdc7
aloysious <alicia.todd.army@gmail.com>
2021-01-14 22:10:24 -0800

    saving

--- js/app.js
+++ js/app.js
@@ -14,29 +14,29 @@
 // 1. get username and offer them a greeting
 // 2. Ask user 5 questions. they MUST accept yes or no in ANY case
 // normalize reponses to reduce coding efforts to .lowerCase();
-// print to console to confirm // 
+// print to console to confirm //
 
 // Ask pnwAnswer: 'Do you think I live in the Pacific Northwest?'
 //  if y:
 //  else if:
-//    else: 
+//    else:
 // Ask cageFightAnswer: 'Would you believe I\'ve ever fought in cages?'
 //  if y:
 //  if n:
-//    else if: 
+//    else if:
 // Ask pokeAnswer: 'Do you think I like poke?'
 //  if y:
 //  if n:
-//    else if: 
+//    else if:
 // Ask skaterAnswer: 'Do you think I could have been a sponsored skateboarder in the 90s?'
 //  if y:
 //  if n:
-//    else if: 
+//    else if:
 // Ask forcedHandAnswer: 'Did someone make you come here?'
 //  if y: ALERT: 'I wonder who you ticked off...'
 //  if n: wow USERNAME i feel honored.
 //    else if: no worries were done USERNAME
-// ------------------------------------------------------------------------------PLAN OF ATTACK/ 
+// ------------------------------------------------------------------------------PLAN OF ATTACK/
 
 var username = prompt('Well hello there wanderer, What\'s your name?');
 alert('Greeting\'s ' + username + ', it\'s a pleasure to meet you!');
@@ -51,7 +51,7 @@ if (pnwAnswer === 'yes' || pnwAnswer === 'y') {
 } else if (pnwAnswer === 'no' || pnwAnswer === 'n') {
   alert('That\'s not right but I can see this turning around for you.');
 } else {
-  alert('Better luck next time ' + username)
+  alert('Better luck next time ' + username);
 }
 
 // question two
@@ -61,9 +61,9 @@ var cageFightAnswer = prompt('Would you believe I\'ve ever fought in cages?').to
 if (cageFightAnswer === 'yes' || cageFightAnswer === 'y') {
   alert('You nailed it!');
 } else if (cageFightAnswer === 'no' || cageFightAnswer === 'n') {
-  alert('That\'s not right but I can still see this turning around for you.')
+  alert('That\'s not right but I can still see this turning around for you.');
 } else {
-  alert('Are you even typing real words in the prompt box? ' + username)
+  alert('Are you even typing real words in the prompt box? ' + username);
 }
 // question three
 var pokeAnswer = prompt('Do you think I like poke?').toLowerCase();
@@ -74,7 +74,7 @@ if (pokeAnswer === 'no' || pnwAnswer === 'n') {
 } else if (pokeAnswer === 'no' || pokeAnswer === 'n') {
   alert('It\'s okay, it\'s not like I told you i didn\'t like it.');
 } else {
-  alert('I\'m not like most Seattleites in this regard ' + username)
+  alert('I\'m not like most Seattleites in this regard ' + username);
 }
 
 // question four
@@ -86,7 +86,7 @@ if (pokeAnswer === 'yes' || skaterAnswer === 'y') {
 } else if (skaterAnswer === 'no' || skaterAnswer === 'n') {
   alert('Sorry ' + username + ', that is incorrect.');
 } else {
-  alert('Are you here against your will ' + username + '?')
+  alert('Are you here against your will ' + username + '?');
 }
 
 // question five
@@ -98,5 +98,15 @@ if (forcedHandAnswer === 'yes' || forcedHandAnswer === 'y') {
 } else if (forcedHandAnswer === 'no' || forcedHandAnswer === 'n') {
   alert('Wow ' + username + ', I feel honored.');
 } else {
-  alert('Don\'t fret, it\'s all over now. ' + 'Welcome ' + username + '.')
+  alert('Don\'t fret, it\'s all over now. ' + 'Welcome ' + username + '.');
 }
+
+// PLAN OF ATTACK------------------------------------------------------------------------------/
+// add another question [question6]
+// Have the user guess a numeric value [value=6]
+// give the user 4 attempts [counter], display an [alert] that lets them know if the guess was too high of too low
+// if correct display [alert]
+//
+//
+//
+//
